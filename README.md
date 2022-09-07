9214b5e945ae2907a92acd674b7aece4f26ad165
jptrzy <jptrzy@gmail.com>
2022-09-07 11:12:23 +0200

    Wails Init

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
 ## About
-Simple Manhwa/Manga/Comic reader written in golang using webview.
-For now this is more of a test for webview then something useful.
+Overcomplicated Manga Viewer (OverManga) is a ...
 
-Why ***OpenManga*** becouse I haven't got any better idea at that moment, but partialy [OpenComic](https://github.com/ollm/OpenComic) project takes it part.
 
-## Installation
 
-## ToDo
-- [ ] Config with config menu
-- [ ] Installation makefile
-- [ ] Windows & ****BSD build
-- [ ] Usage benchmarks/test
-- [ ] Tab turning on/off system
-- [ ] Sidebar
-#### Choice Tab
-- [ ] Tab for files choosing
-#### Reading Tab
-- [ ] Tab for image preview
 
--- main.go
+++ main.go
@@ -1,79 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
+	"embed"
 
-	"github.com/webview/webview"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
 )
 
-type JsonFile struct {
-	Name string `json:"name"`
-	Dir  bool   `json:"dir"`
-}
-
-type JsonResult struct {
-	Files  []JsonFile `json:"files"`
-	Path   string     `json:"path"`
-	Status int        `json:"status"`
-}
-
-func get_files(path string) JsonResult {
-	var status int = 0
-
-	paths, err := ioutil.ReadDir(path)
-	var files []JsonFile = []JsonFile{}
-	if err != nil {
-		fmt.Println("ERROR", err)
-		status = 1
-	} else {
-		for _, f := range paths {
-			files = append(files, JsonFile{Name: f.Name(), Dir: f.IsDir()})
-		}
-	}
-
-	return JsonResult{Files: files, Path: path, Status: status}
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path[5:]
-	// fmt.Fprintf(w, "TEST"+r.Form.Get("p"))
-	b, e := os.ReadFile(path)
-	if e != nil {
-		log.Fatal(e)
-	}
-	w.Write(b)
-}
-
-func init_fileServer() {
-	http.HandleFunc("/view/", viewHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+//go:embed frontend/dist
+var assets embed.FS
 
 func main() {
-	win := webview.New(true)
-	defer win.Destroy()
-	win.SetTitle("Bind Example")
-	win.SetSize(480, 320, webview.HintNone)
+	// Create an instance of the app structure
+	app := NewApp()
 
-	bhtml, _ := os.ReadFile("static/index.html")
-	bcss, _ := os.ReadFile("static/styles.css")
-	bjs, _ := os.ReadFile("static/main.js")
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:            "over-manga",
+		Width:            1024,
+		Height:           768,
+		Assets:           assets,
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
 
-	html := strings.ReplaceAll(string(bhtml), "/* STYLES */", string(bcss))
-	html = strings.ReplaceAll(html, "/* JS */", string(bjs))
-
-	// win.SetHtml(string(html))
-	win.Navigate(`data:text/html,` + strings.ReplaceAll(url.QueryEscape(html), "+", "%20"))
-
-	win.Bind("get_files", get_files)
-
-	go init_fileServer()
-
-	win.Run()
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
