e527e4597019ef0843db760899dea408e5159f8c
jptrzy <jptrzy@gmail.com>
2022-09-09 13:51:25 +0200

    Tabs

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,45 +1,31 @@
 import './style.css';
 import './app.css';
-
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 
 
 // import logo from './assets/images/logo-universal.png';
-import {Greet, ScanDir} from '../wailsjs/go/main/App';
+import {ScanDir} from '../wailsjs/go/main/App';
 import { main } from '../wailsjs/go/models';
 
-let PWD:string = "/home/jptrzy";
-
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement!.value;
-
-    // Check if the input is empty
-    if (name === "") return;
-
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement!.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
-    }
-};
-
+let PWD:string = "/home";
 const SUP_IMG_FORMATS: string[] = [".png", ".webp", ".jpg", ".jpeg"];
 
 function endsWithAny(suffixes: string[], string: string) {
     return suffixes.some(function (suffix) {
         return string.endsWith(suffix);
     });
+}
+
+function update_path(path:string) {
+	PWD = path;
+	$("#path").text(PWD);
+}
+
+window.switch_tab = function(id:string) {
+	$(".select").removeClass("select");	
+
+	$(".tab#" + id).addClass("select");
 }
 
 window.scan_dir = function(path:string, save:boolean){
@@ -59,7 +45,7 @@ ${endsWithAny(SUP_IMG_FORMATS, file.name) ? "img":""}">${file.name}</li>`;
 					$("#dirs").html(html);
 					
 					if (save) {
-						PWD = path;
+						update_path(path);
 					}
 				}
 			}).catch((err) => {
@@ -70,6 +56,7 @@ ${endsWithAny(SUP_IMG_FORMATS, file.name) ? "img":""}">${file.name}</li>`;
 	}	    
 }
 
+update_path(PWD);
 window.scan_dir(PWD, true);
 $(document).on('click', "li.dir", function(e) {
 	let path:string = PWD + "/" + $(this).text();
@@ -83,28 +70,15 @@ $(document).on('click', "li.dir", function(e) {
 	window.scan_dir(path, true);
 });
 
-/*
-document.querySelector('#app')!.innerHTML = `
-    <img id="logo" class="logo">
-      <div class="result" id="result">Please enter your name below 👇</div>
-      <div class="input-box" id="input">
-        <input class="input" id="name" type="text" autocomplete="off" />
-        <button class="btn" onclick="greet()">Greet</button>
-      </div>
-    </div>
-`;
-(document.getElementById('logo') as HTMLImageElement).src = logo;
-*/
-let nameElement = (document.getElementById("name") as HTMLInputElement);
-nameElement.focus();
-let resultElement = document.getElementById("result");
-
+$(document).on('click', "#navbar > li", function() {
+	window.switch_tab($(this).attr("tab")!);
+});
 
 declare global {
     interface Window {
-        greet: () => void;
         scan_dir: (path:string, save:boolean) => void;
-        $: any;
+        switch_tab: (id:string) => void;
+		  $: any;
         jQuery: any;
     }
 }
